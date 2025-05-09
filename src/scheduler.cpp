44edb7446e1fdd515002268b58c642217c59a77b
gezimdibra <32465043+gezimdibra@users.noreply.github.com>
2025-05-08 20:23:09 -0400

    Update scheduler.cpp

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -32,3 +32,37 @@ void Scheduler::clearCurrentProcess() {
 void Scheduler::addToAllProcesses(std::shared_ptr<Process> process) {
     allProcesses.push_back(process);
 }
+#include "scheduler.h"
+
+Scheduler::Scheduler(const std::string& schedulerName, int switchTime)
+    : totalTime(0),
+      cpuBusyTime(0),
+      contextSwitchTime(switchTime),
+      contextSwitchCount(0),
+      isCpuBusy(false),
+      cpuUtilization(0.0),
+      currentProcess(nullptr),
+      name(schedulerName) {
+}
+
+void Scheduler::setCurrentProcess(std::shared_ptr<Process> process) {
+    currentProcess = process;
+    isCpuBusy = (process != nullptr);
+}
+
+std::shared_ptr<Process> Scheduler::getCurrentProcess() const {
+    return currentProcess;
+}
+
+bool Scheduler::hasCpuProcess() const {
+    return isCpuBusy && currentProcess != nullptr;
+}
+
+void Scheduler::clearCurrentProcess() {
+    currentProcess = nullptr;
+    isCpuBusy = false;
+}
+
+void Scheduler::addToAllProcesses(std::shared_ptr<Process> process) {
+    allProcesses.push_back(process);
+}
